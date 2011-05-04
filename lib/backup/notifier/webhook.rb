3968ecd9899379636654c28313b52d8a80563068
Phil McClure <pmcclure@rumblelabs.com>
2011-05-04 20:40:24 +0100

    Changed some of the attributes in the webhook notification

--- lib/backup/notifier/webhook.rb
+++ lib/backup/notifier/webhook.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+
 module Backup
   module Notifier
     class Webhook < Base
@@ -13,12 +14,16 @@ module Backup
       attr_accessor :domain
 
       ##
-      # Controller to post back to
-      attr_accessor :controller
+      # Port to post back to
+      attr_accessor :port
 
       ##
-      # Action to post back to
-      attr_accessor :action
+      # Path to post back to
+      attr_accessor :path
+
+      ##
+      # related object id
+      attr_accessor :related_object_id
 
       ##
       # Hash key for access to your app
@@ -28,9 +33,7 @@ module Backup
       # Instantiates a new Backup::Notifier::Webhook object
       def initialize(&block)
         load_defaults!
-
         instance_eval(&block) if block_given?
-
         set_defaults!
       end
 
@@ -59,17 +62,16 @@ module Backup
       # Sends a tweet informing the user that the backup operation
       # proceeded without any errors
       def notify_success!
-        # return a hash:
-        # backup = {
-        #   :label
-        #   :file_path
-        #   :start_time
-        #   :end_time
-        #   :size
-        #   :compressed?
-        #   :database
-        #   :state (success || failure)
-        # }
+        backup = {
+          :label => backup.label,
+          :file_path => Backup.file,
+          :start_time => backup.time,
+          :state => 'success',
+          :related_object_id => related_object_id
+        }
+
+        http = Net::HTTP.new(domain, port)
+        http.post(path, hash_to_querystring(backup))
       end
 
       ##
@@ -90,7 +92,12 @@ module Backup
 
       end
 
-
+      def hash_to_querystring(hash)
+        hash.keys.inject('') do |query_string, key|
+          query_string << '&' unless key == hash.keys.first
+          query_string << "#{URI.encode(key.to_s)}=#{URI.encode(hash[key])}"
+        end
+      end
     end
   end
 end
